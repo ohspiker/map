29f3ad98c0448870b37e6428a7d5356de41e0873
Olivia Handspiker <handspiker@dal.ca>
2024-06-05 17:56:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let currentCircle = null;
 function initializeMap() {
     map = L.map('map').setView([20, 0], 2);
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/voyager/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
         subdomains: 'abcd',
         maxZoom: 19
